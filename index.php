d8b2790672066288ffe5fefae503bd81e2ac8cef
dwm0013 <dwm0013@mix.wvu.edu>
2021-02-03 20:12:04 -0500

    Lab 2 Finished

--- index.php
+++ index.php
@@ -1,60 +1,41 @@
+<title>Motorsport Reviews</title>
 <main>
     <link rel="stylesheet" href="css/about.css">
 
     <div class="about">
-        <h1>About Us</h1>
-        <p>chimmy chunga bunga bingy wee wee</p>
+        <h1 class="toph">About Us</h1>
+        <p class="topp">The Team behind Motorsport Reviews</p>
     </div>
+
     <div class="row">
         <div class="column">
-            <div class="card">
-                <img src="images/1.jpg">
-                <div class="container">
-                    <h2>CEO</h2>
-                    <p>Harrison Ford</p>
-                    <p>Email: Butter@gmail.com</p>
-                </div>
+            <div class="card" style="float: left; margin-left: 20%;">
+                <img class="cardphoto" src="images/man3.jpg">
+                <h2>CEO</h2>
+                <p>David Mudge</p>
             </div>
         </div>
         <div class="column">
-            <div class="card">
-                <img src="images/2.png">
-                <div class="container">
-                    <h2>CEO</h2>
-                    <p>Harrison Ford</p>
-                    <p>Email: Butter@gmail.com</p>
-                </div>
+            <div class="card" style="float: right; margin-right: 20%">
+                <img class="cardphoto" src="images/man2.jpg">
+                <h2>Partner</h2>
+                <p>Hunter Greenfield</p>
             </div>
         </div>
         <div class="column">
-            <div class="card">
-                <img src="images/3.jpg">
-                <div class="container">
-                    <h2>CEO</h2>
-                    <p>Harrison Ford</p>
-                    <p>Email: Butter@gmail.com</p>
-                </div>
+            <div class="card" style="float: center;">
+                <img class="cardphoto" src="images/man4.jpg">
+                <h2>Partner</h2>
+                <p>Rob Santymire</p>
             </div>
         </div>
-        <div class="column">
-            <div class="card">
-                <img src="images/4.jpg">
-                <div class="container">
-                    <h2>CEO</h2>
-                    <p>Harrison Ford</p>
-                    <p>Email: Butter@gmail.com</p>
-                </div>
-            </div>
-        </div>
-        <div class="column">
-            <div class="card">
-                <img src="images/4.jpg">
-                <div class="container">
-                    <h2>CEO</h2>
-                    <p>Harrison Ford</p>
-                    <p>Email: Butter@gmail.com</p>
-                </div>
-            </div>
+        <div class="para">
+            <p style="padding-top: 10%;">The people behind this site are "The 2B Determined Team"<br> 
+                We all met each other when a tornado hit our city and we all shared the same bunker. <br>
+                While waiting the storm out, we started talking about our love for *insert product*<br>
+                and how we could sell so many of them. It was destine to be. </p>
         </div>
     </div>
+
+
 </main>
